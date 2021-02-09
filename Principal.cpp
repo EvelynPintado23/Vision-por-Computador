5403967b0302c824d1ead156062d37c82782fbf6
Edw1nM4rquez <edwi1999@hotmail.com>
2021-02-09 11:46:51 -0500

    sOL

--- Principal.cpp
+++ Principal.cpp
@@ -1,4 +1,23 @@
-#include "Principal.hpp"
+
+#include <cmath> // Esta librería contiene las funciones para realizar operaciones matemáticas (sin, cos, tan, log, exp, etc.)
+
+// Cuando se carga la cabecer opencv.hpp automáticamente se cargan las demás cabeceras
+//#include <opencv2/opencv.hpp>
+
+#include <opencv2/core/core.hpp> // Contiene los elementos básicos como el objeto Mat (matriz que representa la imagen)
+#include <opencv2/highgui/highgui.hpp> // Contiene los elementos para crear una interfaz gráfica básica
+// OpenCV no está pensado para crear interfaces gráficas potentes. Se centra en la visión artificial y PDI. Si se desea crear una interfaz gráfica completa, se debe usar QT
+
+#include <opencv2/imgcodecs/imgcodecs.hpp> // Contiene las funcionalidad para acceder a los códecs que permiten leer diferentes formatos de imagen (JPEG, JPEG-2000, PNG, TIFF, GIF, etc.)
+
+// Librerías para acceder al video y para poder escribir vídeos en disco
+#include <opencv2/video/video.hpp> 
+#include <opencv2/videoio/videoio.hpp>
+
+#include <opencv2/imgproc/imgproc.hpp> // Librería para realizar operaciones de PDI 
+
+#include <ctime>
+#include <iostream>
 
 using namespace cv;
 using namespace std;
@@ -16,48 +35,37 @@ using namespace std;
 		const int alpha_slider_max10 = 100;
 		const int alpha_slider_max11 = 100;
 		const int alpha_slider_max12 = 100;
-		int alpha_slider1 = 0;
-		int alpha_slider2 = 0;
-		int alpha_slider3 = 0;
-		int alpha_slider4 = 0;
-		int alpha_slider5 = 0;
-		int alpha_slider6 = 0;
-		int alpha_slider7 = 0;
-		int alpha_slider8 = 0; 
-		int alpha_slider9 = 0;
-		int alpha_slider10 = 0;
-		int alpha_slider11 = 0;
-		int alpha_slider12 = 0;
-		double alpha1;
-		double alpha2;
-		double alpha3;
-		double alpha4;
-		double alpha5;
-		double alpha6;
-		double alpha7;
-		double alpha8;
-		double alpha9;
-		double alpha10;
-		double alpha11;
-		double alpha12;
+        
+        int alpha_slider1 ;
+		int alpha_slider2 ;
+		int alpha_slider3 ;
+		int alpha_slider4 ;
+		int alpha_slider5 ;
+		int alpha_slider6 ;
+		int alpha_slider7 ;
+		int alpha_slider8 ; 
+		int alpha_slider9 ;
+		int alpha_slider10;
+		int alpha_slider11;
+		int alpha_slider12;
 //--------------------------------------------------------
 //CascadeClassifier face_cascade;  // Para rostros
 //CascadeClassifier eyes_cascade; // Para ojos
 Mat frame, frameOut, handMask, foreground, fingerCountDebug;
-Prueba skinDetector, backgroundRemover, faceDetector, fingerCount;
-Prueba num;
-
-void on_trackbarHmin( int v, void*) {
+//Prueba skinDetector, backgroundRemover, faceDetector, fingerCount;
+//Prueba num;
+int v=1;
+void on_trackbarHmin( int, void*) {
     // Creamos el structring element (que puede ser una cruz, un rectángulo o una elipse):
     // MORPH_CROSS, MORPH_RECT, MORPH_ELLIPSE
     //Mat elemento = getStructuringElement(MORPH_CROSS, Size(alpha_slider+1,alpha_slider+1), Point(-1,-1));
     // Aplicamos la operación de dilatación
     //morphologyEx(imagen, frame, MORPH_DILATE,elemento); 
-    cvtColor(frame,frame,COLOR_BGR2HSV);
+    //cvtColor(frame,frame,COLOR_BGR2HSV);
 
     cout << "trackbarHmin " << v << endl;   
 }
-void on_trackbarSmin( int v, void *) {
+void on_trackbarSmin( int, void *) {
     // Creamos el structring element (que puede ser una cruz, un rectángulo o una elipse):
     // MORPH_CROSS, MORPH_RECT, MORPH_ELLIPSE
     //Mat elemento = getStructuringElement(MORPH_CROSS, Size(alpha_slider+1,alpha_slider+1), Point(-1,-1));
@@ -65,7 +73,7 @@ void on_trackbarSmin( int v, void *) {
     //morphologyEx(imagen, frame, MORPH_DILATE,elemento);
     cout << "trackbarsmin " << v << endl; 
 }
-void on_trackbarVmin( int v, void *) {
+void on_trackbarVmin( int, void *) {
     // Creamos el structring element (que puede ser una cruz, un rectángulo o una elipse):
     // MORPH_CROSS, MORPH_RECT, MORPH_ELLIPSE
     //Mat elemento = getStructuringElement(MORPH_CROSS, Size(alpha_slider+1,alpha_slider+1), Point(-1,-1));
@@ -74,7 +82,7 @@ void on_trackbarVmin( int v, void *) {
     cout << "trackbarvmin " << v << endl; 
     
 }
-void on_trackbarHmax( int v, void *) {
+void on_trackbarHmax( int, void *) {
     // Creamos el structring element (que puede ser una cruz, un rectángulo o una elipse):
     // MORPH_CROSS, MORPH_RECT, MORPH_ELLIPSE
     //Mat elemento = getStructuringElement(MORPH_CROSS, Size(alpha_slider+1,alpha_slider+1), Point(-1,-1));
@@ -83,7 +91,7 @@ void on_trackbarHmax( int v, void *) {
    
     cout << "trackbarHmax " << v << endl; 
 }
-void on_trackbarSmax( int v, void *pP) {
+void on_trackbarSmax( int, void *pP) {
     // Creamos el structring element (que puede ser una cruz, un rectángulo o una elipse):
     // MORPH_CROSS, MORPH_RECT, MORPH_ELLIPSE
     //Mat elemento = getStructuringElement(MORPH_CROSS, Size(alpha_slider+1,alpha_slider+1), Point(-1,-1));
@@ -91,7 +99,7 @@ void on_trackbarSmax( int v, void *pP) {
     //morphologyEx(imagen, frame, MORPH_DILATE,elemento);
     cout << "trackbarsmax " << v << endl; 
 }
-void on_trackbarVmax( int v, void *) {
+void on_trackbarVmax( int, void *) {
     // Creamos el structring element (que puede ser una cruz, un rectángulo o una elipse):
     // MORPH_CROSS, MORPH_RECT, MORPH_ELLIPSE
     //Mat elemento = getStructuringElement(MORPH_CROSS, Size(alpha_slider+1,alpha_slider+1), Point(-1,-1));
@@ -100,7 +108,7 @@ void on_trackbarVmax( int v, void *) {
      
     cout << "trackbarvmax " << v << endl; 
 }
-void on_trackbarYmin( int v, void *) {
+void on_trackbarYmin( int, void *) {
     // Creamos el structring element (que puede ser una cruz, un rectángulo o una elipse):
     // MORPH_CROSS, MORPH_RECT, MORPH_ELLIPSE
     //Mat elemento = getStructuringElement(MORPH_CROSS, Size(alpha_slider+1,alpha_slider+1), Point(-1,-1));
@@ -108,7 +116,7 @@ void on_trackbarYmin( int v, void *) {
     //morphologyEx(imagen, frame, MORPH_DILATE,elemento);
     cout << "on_trackbarYmin " << v << endl; 
 }    
-void on_trackbarCrmin( int v, void *) {
+void on_trackbarCrmin( int, void *) {
     // Creamos el structring element (que puede ser una cruz, un rectángulo o una elipse):
     // MORPH_CROSS, MORPH_RECT, MORPH_ELLIPSE
     //Mat elemento = getStructuringElement(MORPH_CROSS, Size(alpha_slider+1,alpha_slider+1), Point(-1,-1));
@@ -116,7 +124,7 @@ void on_trackbarCrmin( int v, void *) {
     //morphologyEx(imagen, frame, MORPH_DILATE,elemento);
     cout << "on_trackbarcrmin " << v << endl; 
 }
-void on_trackbarCbmin( int v, void *) {
+void on_trackbarCbmin( int, void *) {
     // Creamos el structring element (que puede ser una cruz, un rectángulo o una elipse):
     // MORPH_CROSS, MORPH_RECT, MORPH_ELLIPSE
     //Mat elemento = getStructuringElement(MORPH_CROSS, Size(alpha_slider+1,alpha_slider+1), Point(-1,-1));
@@ -125,7 +133,7 @@ void on_trackbarCbmin( int v, void *) {
     cout << "on_trackbarcbmin " << v << endl; 
     
 }
-void on_trackbarYmax( int v, void *) {
+void on_trackbarYmax( int, void *) {
     // Creamos el structring element (que puede ser una cruz, un rectángulo o una elipse):
     // MORPH_CROSS, MORPH_RECT, MORPH_ELLIPSE
     //Mat elemento = getStructuringElement(MORPH_CROSS, Size(alpha_slider+1,alpha_slider+1), Point(-1,-1));
@@ -133,7 +141,7 @@ void on_trackbarYmax( int v, void *) {
     //morphologyEx(imagen, frame, MORPH_DILATE,elemento);    
     cout << "on_trackbarYmax " << v << endl; 
 }
-void on_trackbarCrmax( int v, void *) {
+void on_trackbarCrmax( int, void *) {
     // Creamos el structring element (que puede ser una cruz, un rectángulo o una elipse):
     // MORPH_CROSS, MORPH_RECT, MORPH_ELLIPSE
     //Mat elemento = getStructuringElement(MORPH_CROSS, Size(alpha_slider+1,alpha_slider+1), Point(-1,-1));
@@ -141,7 +149,7 @@ void on_trackbarCrmax( int v, void *) {
     //morphologyEx(imagen, frame, MORPH_DILATE,elemento);
     cout << "on_trackbarcrmax " << v << endl; 
 }
-void on_trackbarCbmax( int v, void *) {
+void on_trackbarCbmax( int, void *) {
 
     // Creamos el structring element (que puede ser una cruz, un rectángulo o una elipse):
     // MORPH_CROSS, MORPH_RECT, MORPH_ELLIPSE
@@ -156,9 +164,7 @@ void on_trackbarCbmax( int v, void *) {
 
 int main(int, char**) {
 
-
 	VideoCapture videoCapture(0);
-
 
     namedWindow("Original", WINDOW_AUTOSIZE);
     namedWindow("output", WINDOW_AUTOSIZE);
@@ -166,19 +172,19 @@ int main(int, char**) {
     namedWindow("handMask", WINDOW_AUTOSIZE);
     namedWindow("handDetection", WINDOW_AUTOSIZE);
     
-     alpha_slider1 = 0;
-     alpha_slider2 = 0;
-     alpha_slider3 = 0;
-     alpha_slider4 = 0;
-     alpha_slider5 = 0;
-     alpha_slider6 = 0;
-     alpha_slider7 = 0;
-     alpha_slider8 = 0;
-     alpha_slider9 = 0;
-     alpha_slider10 = 0;
-     alpha_slider11 = 0;
-     alpha_slider12 = 0;
-
+    int alpha_slider1 = 0;
+    int alpha_slider2 = 0;
+    int alpha_slider3 = 0;
+    int alpha_slider4 = 0;
+    int alpha_slider5 = 0;
+    int alpha_slider6 = 0;
+    int alpha_slider7 = 0;
+    int alpha_slider8 = 0; 
+    int alpha_slider9 = 0;
+    int alpha_slider10 = 0;
+    int alpha_slider11 = 0;
+    int alpha_slider12 = 0;
+        
     char TrackbarName1[50];
     char TrackbarName2[50];
     char TrackbarName3[50];
@@ -219,7 +225,7 @@ int main(int, char**) {
     createTrackbar(TrackbarName12, "Original", &alpha_slider12,  alpha_slider_max12,  on_trackbarCbmax);
 
     if(videoCapture.isOpened()){
-        Prueba detector;
+        //Prueba detector;
         
         while (3==3) {
             videoCapture >> frame;
@@ -227,7 +233,7 @@ int main(int, char**) {
             frameOut = frame.clone();
 
             
-             on_trackbarHmin( alpha_slider1, 0);
+             /*on_trackbarHmin( alpha_slider1, 0);
              on_trackbarSmin( alpha_slider2, 0);
              on_trackbarVmin( alpha_slider3, 0);
              on_trackbarHmax( alpha_slider4, 0);
@@ -238,15 +244,15 @@ int main(int, char**) {
              on_trackbarCbmin( alpha_slider9, 0);
              on_trackbarYmax( alpha_slider10, 0);
              on_trackbarCrmax( alpha_slider11, 0);
-             on_trackbarCbmax( alpha_slider12, 0);
+             on_trackbarCbmax( alpha_slider12, 0);*/
             imshow("Original", frame);
 
 
-            detector.drawSkinColorSampler(frameOut);
+            /*detector.drawSkinColorSampler(frameOut);
             foreground = detector.getForeground(frame);
             detector.removeFaces(frame, foreground);
             handMask = detector.getSkinMask(foreground);
-            fingerCountDebug = detector.findFingersCount(handMask, frameOut); 
+            fingerCountDebug = detector.findFingersCount(handMask, frameOut); */
            
             
             imshow("handMask", handMask);
@@ -258,10 +264,10 @@ int main(int, char**) {
 
             if (key == 27) // esc
                 break;
-            else if (key == 98) // b
-                detector.calibrate1(frame);
-            else if (key == 115) // s
-                detector.calibrate(frame);
+            //else if (key == 98) // b
+               // detector.calibrate1(frame);
+            //else if (key == 115) // s
+              //  detector.calibrate(frame);
         }
 
         videoCapture.release();
